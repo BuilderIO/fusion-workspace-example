15eee08833fcfe64da572804e497dae81d0f3636
Steve Sewell <steve@builder.io>
2025-05-23 16:27:35 -0700

    ok

---
+++ app/src/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Hello World!</h1>
+      <p>Welcome to our simple React app with React Router</p>
+    </div>
+  )
+}
+
+export default HomePage 
